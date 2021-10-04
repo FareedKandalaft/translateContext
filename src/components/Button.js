c5c01db44ebf39977ae0cb4775e99adf5e0a967b
FareedKandalaft <fareed@spectrecoder.com>
2021-10-04 19:49:35 -0400

    Finished Sample

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
-export default class Button extends Component {
+class Button extends Component {
+  // important! must be static variable named contextType
+  static contextType = LanguageContext;
+
   render() {
-    return <button className='ui button primary'>Submit</button>;
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    return <button className='ui button primary'>{text}</button>;
   }
 }
+// Alternatively you can hook to the contect
+// by declaration AFTER the class def
+// Button.contextType = LanguageContext;
+
+export default Button;
---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,3 @@
+import React from 'react';
+// Here is a context created and set with a default value
+export default React.createContext('english+');
