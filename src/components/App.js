f816217ca50c92bd17199fb3473e9825273ed91f
FareedKandalaft <fareed@spectrecoder.com>
2021-10-03 20:59:15 -0400

    Main components and stylesheet set up.

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+
+class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+  render() {
+    return (
+      <div className='ui container'>
+        <div>
+          Select a Language:
+          <i
+            className='flag us'
+            onClick={() => this.onLanguageChange('english')}
+          ></i>
+          <i
+            className='flag nl'
+            onClick={() => this.onLanguageChange('dutch')}
+          ></i>
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
+
+export default App;
